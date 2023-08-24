08a00ed5cdfd1ac49850548eafe5d18aa1b47238
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-08-24 18:40:47 -0300

    base, index and header template

--- apps/myself/views.py
+++ apps/myself/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Myself
 
 # Create your views here.
-def home(request):
+
+
+def index(request):
     myself = Myself.objects.first()
     ctx = {"myself": myself}
-    return render(request, "base.html", ctx)
+    return render(request, "index.html", ctx)
