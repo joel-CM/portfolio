52bc22a5d8b3ce6665b80118d5ad957607ad380d
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-08-24 16:39:45 -0300

    myself and project models

--- apps/myself/models.py
+++ apps/myself/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Myself(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    about_me = models.TextField()
+    photo = models.ImageField(blank=True)
--- apps/project/models.py
+++ apps/project/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    source = models.CharField(blank=True)
+    site = models.CharField(blank=True)
