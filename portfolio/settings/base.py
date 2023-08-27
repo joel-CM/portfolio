19c55a1c05497c3bc14633225dbfd60b62463e3d
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-08-27 13:56:47 -0300

    some fixes

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -102,8 +102,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = tuple([os.path.join(BASE_DIR, "public/static/")])
-STATIC_ROOT = os.path.join(BASE_DIR, "public/staticfiles")
+STATICFILES_DIRS = tuple([os.path.join(BASE_DIR, "public/staticfiles/")])
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
