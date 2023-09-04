a06470d4f91c778efb9ecdb16128ae0fc3a48a55
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-04 16:29:26 -0300

    add github and linkedin url to myself model

--- apps/myself/models.py
+++ apps/myself/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Myself(models.Model):
     name = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
     about_me = models.TextField()
     photo = models.ImageField(blank=True)
+    github = models.CharField(max_length=500, blank=True)
+    linkedin = models.CharField(max_length=500, blank=True)
