e5bc97289cbb59df74a429b11383321c49610cbd
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-09-04 16:13:30 -0300

    project-card component

--- apps/myself/views.py
+++ apps/myself/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Myself
+from apps.project.models import Project
 
 # Create your views here.
 
 
 def index(request):
     myself = Myself.objects.first()
-    ctx = {"myself": myself}
+    projects = Project.objects.all()
+    ctx = {"myself": myself, "projects": projects}
     return render(request, "index.html", ctx)
